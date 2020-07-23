4a66a163b2d52f2909e0525ebddc8eb98cce351c
mstrhakr <37352843+mstrhakr@users.noreply.github.com>
2020-07-22 23:00:08 -0400

    Create plexpreroll.ps1
    initial upload for script

---
+++ plexpreroll.ps1
@@ -0,0 +1,9 @@
+﻿#('\config\Preroll\' + (Get-ChildItem -Path \\192.168.1.150\appdata\binhex-plexpass\Preroll\ -Name)) -join(';')
+#plex preroll file name list generator - manually configured for now
+#$path = Read-Host "Enter preroll local path"
+#$plexpath = Read-Host "Enter plex relative preroll path"
+foreach ($file in (Get-ChildItem -Path \\192.168.1.150\appdata\binhex-plexpass\Preroll\ -Name)) {
+    [array]$list += ('/config/Preroll/' + $file)
+}
+$output = $list -join(';')
+Write-Host $output
